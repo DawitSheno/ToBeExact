30c4020ff0872f1b5e51e8387cf27f4845cefb1e
DawitSheno <57653398+DawitSheno@users.noreply.github.com>
2019-12-25 22:57:12 -0500

    This is Mara, adding another for loop!

---
+++ QA_Basics/src/simple_testCase/AddingCode.java
@@ -0,0 +1,15 @@
+package simple_testCase;
+
+public class AddingCode {
+
+	public static void main(String[] args) {
+      
+		
+		for(int i =1; i<=2; i++) {
+			
+			System.out.println(" i love My Pets");
+			
+		}
+	}
+
+}
